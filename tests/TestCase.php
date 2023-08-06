450b54d2a0fb81ef40926134db74024a37e83ead
jeanlucnguyen <jeanlucnguyen@users.noreply.github.com>
2023-08-06 14:37:30 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Jeanlucnguyen\LaratranslateSdk\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Jeanlucnguyen\LaratranslateSdk\LaratranslateSdkServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
